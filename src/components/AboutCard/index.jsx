ac4ce29d21545d441e4cc923b46b6531763f6a87
Abhishek Jha <abhjha@deloitte.com>
2018-02-12 01:27:48 +0530

    style
    style

--- src/components/AboutCard/index.jsx
+++ src/components/AboutCard/index.jsx
@@ -4,10 +4,10 @@ import './AboutCard.css';
 class AboutCard extends Component {
   render() {
     return (
-        <div id="aboutCard" className="col-lg-8">                
-            <h2 className="section-heading">We've got what you need!</h2>  
-            <hr className="primary" />     
-            <p className="text-faded">
+        <div id="aboutCard" className="about-card bground-primary col-lg-12">                
+            <h2 className="col col-md-18 text-white">We've got what you need!</h2>  
+            <hr className="col col-md-8 text-white" />     
+            <p className="col col-md-8 text-white">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium similique eos eligendi error veritatis harum, sed aliquid totam, quam minus neque enim est architecto, ad. Maiores, aliquid, vitae!
             </p>
         </div>
--- src/components/ServicesCard/index.jsx
+++ src/components/ServicesCard/index.jsx
@@ -4,23 +4,23 @@ import './ServicesCard.css';
 class ServicesCard extends Component {
   render() {
     return (
-      <div id="servicesCard" className="col-lg-8">                
-          <h2 className="section-heading">At your service</h2>   
+      <div id="servicesCard" className="services-card col-lg-12">                
+          <h2>At your service</h2>   
           <hr className="primary" /> 
           <div className="container">
             <div className="row">            
-              <div className="col-lg-3 col-md-6 text-center">
+              <div className="col-md-6">
                   <div className="service-box">
                       <i className="fa fa-4x fa-diamond wow bounceIn text-primary"></i>
-                      <h3>Sturdy Templates</h3>                        
-                        <p className="text-muted">Our templates are updated regularly so they don't break.</p>
+                      <h3>Lorem ipsum</h3>                        
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
                   </div>
               </div>         
-              <div className="col-lg-3 col-md-6 text-center">
+              <div className="col-md-6">
                   <div className="service-box">
                       <i className="fa fa-4x fa-heart wow bounceIn text-primary"></i>
-                      <h3>Made with Love</h3>
-                        <p className="text-muted">You have to make your websites with love these days!</p>
+                      <h3>Lorem ipsum</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
                   </div>
               </div>
             </div>
